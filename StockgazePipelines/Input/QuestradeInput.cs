c412b3180509d7dc8f3e7256bbf7c25be9fcc8a8
Philippe Deslongchamps <pdeslongchamps@genetec.com>
2020-09-21 17:46:47 -0400

    Added pipelines

---
+++ StockgazePipelines/Input/QuestradeInput.cs
@@ -0,0 +1,48 @@
+﻿//  ==========================================================================
+//  Copyright (C) 2020 by Genetec, Inc.
+//  All rights reserved.
+//  May be used only in accordance with a valid Source Code License Agreement.
+//  ==========================================================================
+
+using System;
+using System.Dynamic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Questrade.BusinessObjects.Entities;
+using Stockgaze.Core.Repositories;
+using Stockgaze.Core.Services;
+
+namespace StockgazePipelines.Input
+{
+
+    public interface IPipelineOperation {}
+    
+    public interface IQuestradeInput<T>
+    {
+
+    }
+
+    public class EquitySymbolDataProvider : IQuestradeInput<EquitySymbol>
+    {
+
+        private readonly SymbolSearchService _searchService;
+
+        private BufferBlock<EquitySymbol> _block;
+
+        public EquitySymbolDataProvider(SymbolSearchService searchService)
+        {
+            _searchService = searchService;
+            _block = new BufferBlock<EquitySymbol>();
+        }
+        
+        public BufferBlock<EquitySymbol> Block => _block;
+
+        public async Task Search(string searchTerm)
+        {
+            var symbols = await _searchService.Search(searchTerm);
+            symbols.ForEach(symbol=>_block.Post(symbol));
+        }
+
+    }
+
+}
---
+++ StockgazePipelinesTest/Tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Questrade.BusinessObjects.Entities;
+using Stockgaze.Core;
+using Stockgaze.Core.Login;
+using Stockgaze.Core.Services;
+using StockgazePipelines.Input;
+using Xunit;
+
+namespace StockgazePipelinesTest
+{
+
+    public class Tests
+    {
+
+        private GazerVM m_gazer;
+
+        private QuestradeAccountManager m_accountManager;
+
+        private SymbolSearchService m_symbolSearchService;
+
+        public Tests()
+        {
+            m_accountManager = new QuestradeAccountManager();
+            m_symbolSearchService = new SymbolSearchService(m_accountManager);
+            m_gazer = new GazerVM();
+            m_gazer.Initialize();
+        }
+        
+        [Fact]
+        public async Task Test1()
+        {
+            // Arrange
+            var dataService = new EquitySymbolDataProvider(m_symbolSearchService);
+            var input = dataService.Block;
+            var filter = new TransformBlock<EquitySymbol,EquitySymbol>(
+                s => s.m_isTradable ? s : null);
+            var ignore = DataflowBlock.NullTarget<EquitySymbol>();
+            var output = new ActionBlock<EquitySymbol>(
+                s => {
+                    Console.WriteLine($"{s.m_symbol} {s.m_listingExchange} {s.m_description}");
+                });
+            input.LinkTo(filter);
+            filter.LinkTo(output, s => s != null);
+            filter.LinkTo(ignore);
+            
+            // Act
+            if (m_accountManager.TryRefreshAuth())
+            {
+                await dataService.Search("CGC");
+            }
+            else
+            // Assert
+            Assert.True(false);
+        }
+
+    }
+
+}
