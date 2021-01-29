f427358ad5aa413178dba582edb3522267aaf95c
Philippe Deslongchamps <pdeslongchamps@genetec.com>
2021-01-28 22:32:34 -0500

    idk

---
+++ OptionGaze/Pipelines/PipelinesUserControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿//  ==========================================================================
+//  Copyright (C) 2020 by Genetec, Inc.
+//  All rights reserved.
+//  May be used only in accordance with a valid Source Code License Agreement.
+//  ==========================================================================
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using Stockgaze.Core.Pipelines;
+
+namespace OptionGaze.Pipelines
+{
+
+    public partial class PipelinesUserControl : UserControl
+    {
+
+        private PipelinesUCVM m_pipelinesUCVM;
+
+        public PipelinesUserControl() { try
+            {
+                InitializeComponent();
+                m_pipelinesUCVM = new PipelinesUCVM();
+                DataContext = m_pipelinesUCVM;
+                m_pipelinesUCVM.Initialize();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"message:{e.Message} stacktrace:{e.StackTrace}");
+            }}
+
+    }
+
+}
---
+++ OptionGaze/Scheduling/ScheduleDialogUserControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using Stockgaze.Core.Scheduling;
+
+namespace OptionGaze.Scheduling
+{
+
+    public partial class ScheduleDialogUserControl : UserControl
+    {
+
+        private readonly SchedulingUCVM m_schedulingUCVM;
+
+        public ScheduleDialogUserControl()
+        {
+            InitializeComponent(); 
+            m_schedulingUCVM = new SchedulingUCVM();
+            DataContext = m_schedulingUCVM;
+        }
+
+    }
+
+}
